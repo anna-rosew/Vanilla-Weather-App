12fb543b1eb650db0f01f95990978cfd9e6106be
Anna <annarosewain9@gmail.com>
2023-04-18 18:41:28 +0100

    Added JS to show time, date and temp in innerHTML.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,44 @@
+function formatDayTime(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
 
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let dateNo = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+
+  return `${dateNo} ${month} ${year}`;
+}
+
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#strong-temp");
+  let conditionsElement = document.querySelector("#conditions");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  let dayElement = document.querySelector("#dayTime");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let cityElement = document.querySelector("#city");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  conditionsElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dayElement.innerHTML = formatDayTime(response.data.dt * 1000);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  cityElement.innerHTML = response.data.name;
+}
+
+let cityName = "New York";
+let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
