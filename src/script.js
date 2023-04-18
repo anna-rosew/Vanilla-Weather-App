753df99e730304b973a955d0d6abf210ddd7cce5
Anna <annarosewain9@gmail.com>
2023-04-18 19:52:07 +0100

    Search Form

--- src/script.js
+++ src/script.js
@@ -3,7 +3,22 @@ function formatDayTime(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  let day = date.getDay();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
   return `${day} ${hours}:${minutes}`;
 }
@@ -11,22 +26,36 @@ function formatDayTime(timestamp) {
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let dateNo = date.getDate();
-  let month = date.getMonth();
+  let months = [
+    "Jan.",
+    "Feb.",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug.",
+    "Sept.",
+    "Oct.",
+    "Nov.",
+    "Dec.",
+  ];
+  let month = months[date.getMonth()];
   let year = date.getFullYear();
 
   return `${dateNo} ${month} ${year}`;
 }
 
 function displayTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#strong-temp");
   let conditionsElement = document.querySelector("#conditions");
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
   let dayElement = document.querySelector("#dayTime");
   let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
   let cityElement = document.querySelector("#city");
+  let iconElement = document.querySelector(`#icon`);
+  let iconCode = response.data.weather[0].icon;
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   conditionsElement.innerHTML = response.data.weather[0].description;
@@ -35,10 +64,26 @@ function displayTemperature(response) {
   dayElement.innerHTML = formatDayTime(response.data.dt * 1000);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   cityElement.innerHTML = response.data.name;
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let cityName = "New York";
-let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
